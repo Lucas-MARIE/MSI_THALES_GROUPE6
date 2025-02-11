0a6c8c30b560336dbbf9da6073a4df29c7c689bd
Lorenzo JACCAUD-GODEFROY <74664396+lorenzojg@users.noreply.github.com>
2025-02-11 09:45:55 +0100

    Update extracteur.py
    Test intégration groq

--- extracteur.py
+++ extracteur.py
@@ -1,29 +1,34 @@
-import openai
+from groq import Groq
 import json
 
+client = Groq(
+    api_key="clegroq",
+)
 
-openai.api_key = 'nom de clé'
-
-def chat_with_gpt(prompt):
+def chat_with_groq(l_prompt):
+    response = {}
     try:
-        response = openai.Completion.create(
-            engine="gpt-3.5-turbo",  # Ou "gpt-4" selon le modèle que tu utilises
-            prompt=prompt,
-            max_tokens=150,  # Le nombre maximum de tokens dans la réponse
-            n=1,  # Nombre de réponses générées
-            stop=None,  # Tu peux définir un critère d'arrêt si besoin
-            temperature=0.7  # Plus la température est élevée, plus la réponse est créative
-        )
-        
+        for texte in l_prompt :
+            response[texte]=[]
+            r = client.chat.completions.create(
+            messages=[
+                {
+                    "role": "user",
+                    "content": texte,
+                }
+            ],
+            model="llama3-8b-8192",
+            )
+            response[texte].append(r.choices[0].message.content)
         # Extraire et retourner la réponse du modèle
-        return response.choices[0].text.strip()
+        return response
 
     except Exception as e:
         print(f"Une erreur s'est produite : {e}")
         return None
 
 # On ouvre le fichier JSON
-with open('articles.json', 'r') as file:
+with open('/Users/lorenzojaccaudgodefroy/Desktop/Excel/data/articles.json', 'r') as file:
     data = json.load(file)  # Charger le fichier JSON sous forme de liste de dictionnaires
 
 # On crée le fichicher CSV
@@ -32,12 +37,16 @@ csv_file = 'resultats_export.csv'
 # On ouvre le fichier CSV dans le but d'écrire les données des articles
 with open(csv_file, mode='w', encoding='utf-8') as file:
     # On commence par écrire l'entête
-    file.write('URL;PublicationTypes;publicationDate;Title;Abstract\n')
+    file.write('URL;PublicationTypes;publicationDate;Title;Facteurs_dev;Facteurs_precisions;Pays;Date_source;Année_source;Méthode;Abstract\n')
+    file_content = ""
 
     # On vérifie que le JSON est bien sous forme de liste
     if isinstance(data, list):
         # On récupère les données de chacun des articles récoltés
+        count = 1
         for item in data:
+            print("Data n°"+str(count)+"/"+str(len(data)))
+
             # On extrait les données qui nous interessent
             url = item.get('url', '')
             title = item.get('title', '')
@@ -68,10 +77,28 @@ with open(csv_file, mode='w', encoding='utf-8') as file:
             #if not(factor_dev) or not(factor_precisions) or not(country) or not(data_s) or not(year_s) or not(methode):
                 #print("Problème avec l'un des prompts.")
             #else :
-            line = f'{url};{publicationTypes};{publicationDate};{title};{abstract}\n'
-            # On écrit la ligne dans le fichier CSV
-            file.write(line)
 
+            t_rep=[factor_dev,factor_precisions,country,data_s,year_s,methode]
+            requ = chat_with_groq(t_rep)
+
+            i=0
+            first=True
+            for el in requ:
+                for rows in requ[el]:
+                    if first :
+                        t_rep[i]=rows
+                        first=False
+                    else :
+                        t_rep[i]+=rows
+                i+=1
+                first=True
+
+
+
+            file_content += f'{url};{publicationTypes};{publicationDate};{title};{t_rep[0]};{t_rep[1]};{t_rep[2]};{t_rep[3]};{t_rep[4]};{t_rep[5]};{abstract}\n'
+            count+=1
+
+        file.write(file_content)
         print(f"Les données ont été exportées avec succès dans '{csv_file}'.")
     else:
         print("Le fichier JSON ne contient pas une liste.")
